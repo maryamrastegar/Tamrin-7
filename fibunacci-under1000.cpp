5da50f22e49a58d6c79bf78c8e80ebc9a749e77e
maryamrastegar <76528756+maryamrastegar@users.noreply.github.com>
2021-12-08 17:45:39 +0330

    Add files via upload

---
+++ fibunacci-under1000.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int fib(int x) {
+	if ((x == 1) || (x == 0)) {
+		return(x);
+	}
+	else {
+		return(fib(x - 1) + fib(x - 2));
+	}
+}
+int main() {
+	int i = 0;
+	
+	cout << "\nFibonnaci Series : ";
+	while(fib(i)<1000){
+		cout << " " << fib(i);
+		i++;
+	}
+	system("pause");
+	return 0;
+}
